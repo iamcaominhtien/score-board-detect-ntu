591f9112305330921c29474b0f27480a7e384055
Tien Minh Cao <153573273+infocaominhtien@users.noreply.github.com>
2024-03-15 16:34:59 +0700

    create a new test file for detecting table API

---
+++ research_stuff/test/test_detect_api.py
@@ -0,0 +1,19 @@
+import os
+
+import cv2
+
+from cell_module.detect_api.api import DetectTableAPI
+from my_utils.logging_setup import Logger
+from my_utils.path_helper import path_images
+
+IMAGES_PATH = path_images()
+
+
+def test_detect_api():
+	for k in range(1, 15):
+		file = os.path.join(IMAGES_PATH, "Anh{}.jpg".format(k))
+		IMAGE = cv2.imread(file)
+		Logger.info('Test image: {}'.format(file))
+		detect_table_api = DetectTableAPI(IMAGE)
+		Logger.info(f'\n{detect_table_api.detect()}')
+		Logger.info(f'\n{"-" * 50}\n')
